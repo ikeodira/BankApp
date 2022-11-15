bb2e96542f562c4001acaf02181ac9e6fc9382a1
Ike Odira <odiraike8@gmail.com>
2022-11-14 19:40:24 -0800

    Complete CardDeal Section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {card} from "../assets";
+import styles, {layout} from "../style";
+import Button from "./Button";
 
 function CardDeal() {
   return (
-    <div>CardDeal</div>
+    <section className={layout.sectionImg}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden"/> in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non sint doloremque debitis odit quidem dignissimos autem, ea expedita qui id similique suscipit ullam ipsa reiciendis!</p>
+        <Button styles="mt-10"/>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]'/>   
+      </div>      
+    </section>
   )
 }
 
